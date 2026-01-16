c2fcca95fb02a761bbce479601ca88dad9e2a4ea
yubbiXploit86 <ayyubbiiiii@gmail.com>
2026-01-17 00:43:54 +0700

    Update crypto_core.py

--- crypto_core.py
+++ crypto_core.py
@@ -1,11 +1,11 @@
 import os
+import cryptography
 from cryptography.fernet import Fernet
 
-# Generate a unique encryption key (dummy, akan diganti dari main.py)
+# Generate a unique encryption key
 encryption_key = Fernet.generate_key()
 
 def encrypt_file(file_path):
-    """Encrypts a file using the global encryption_key"""
     with open(file_path, 'rb') as file:
         data = file.read()
     fernet = Fernet(encryption_key)
@@ -14,7 +14,6 @@ def encrypt_file(file_path):
         file.write(encrypted_data)
 
 def decrypt_file(file_path):
-    """Decrypts a file using the global encryption_key"""
     with open(file_path, 'rb') as file:
         data = file.read()
     fernet = Fernet(encryption_key)
