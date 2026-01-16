4054f69918ed8e4e702475f2f32fcaa8880c2a9c
yubbiXploit86 <ayyubbiiiii@gmail.com>
2026-01-16 23:30:54 +0700

    Create crypto_core.py

---
+++ crypto_core.py
@@ -0,0 +1,22 @@
+import os
+import cryptography
+from cryptography.fernet import Fernet
+
+# Generate a unique encryption key
+encryption_key = Fernet.generate_key()
+
+def encrypt_file(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    fernet = Fernet(encryption_key)
+    encrypted_data = fernet.encrypt(data)
+    with open(file_path, 'wb') as file:
+        file.write(encrypted_data)
+
+def decrypt_file(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    fernet = Fernet(encryption_key)
+    decrypted_data = fernet.decrypt(data)
+    with open(file_path, 'wb') as file:
+        file.write(decrypted_data)
